7d6a61c88c0115d78d65f517de980cc9151cda73
Cotekino <150658630+Cotekino@users.noreply.github.com>
2023-11-15 11:48:48 +0100

    Add files via upload

---
+++ Picasso.py
@@ -0,0 +1,20 @@
+nom = "Picasso"  # déclaration...
+nom = "Cyril"   # juste re-assignation.
+print("\n", nom, "\n")
+
+"""
+Commentaire
+multiligne
+"""
+nom = 10  # re-déclaration avec un type différent
+nom = 'carlo'  # même que "carlo"
+
+giusto = True  # ou False
+prenom = None  # pas de valeur;
+# c'est un type à part entière, utilisé pour des comparaisons.
+
+giusto = "100" < "50"  # compar. entre strings: selon l'ordre alphabetique.
+# attention: majuscule < minuscule (ordre dans le code ascii)
+print(giusto, "\n")
+ 
+# print("\n", nom.upper, "\n") # retourne une COPIE de la variable, convertie en majuscule
---
+++ Test_if.py
@@ -0,0 +1,9 @@
+login = "Gulp"
+PassW = "Quack"
+
+if login != "Admin":
+    print("ID inconnu")
+elif PassW != "admin":
+    print("Mot de passe invalide")
+else:
+    print("yeah!")
