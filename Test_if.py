970745934ced8e22de443f08b8fe74e602a59c03
Cotekino <150658630+Cotekino@users.noreply.github.com>
2023-11-15 12:15:39 +0100

    Add files via upload

--- Test_if.py
+++ Test_if.py
@@ -1,21 +1,21 @@
-login = "JY"
-PassW = "BG77540"
+login = "Admin"
+PassW = "chaussette"
 
 print("\n")
 
 if login == "Admin":
-    if PassW == "admin":
-        print("bienvenu ", login," !")
+    if (PassW == "admin") or (PassW == "supermdp") or (PassW == "chaussette"):
+        print("bienvenu", login,"!")
     else:
         print(login, ": Mot de passe invalide")
 elif login == "rmechety":
     if PassW == "mdp":
-        print("bienvenu ", login," !")
+        print("bienvenu", login,"!")
     else:
         print(login, ": Mot de passe invalide")
 elif login == "JY":
     if PassW == "BG77540":
-        print("bienvenu ", login," !")
+        print("bienvenu", login,"!")
     else:
         print(login, ": Mot de passe invalide")
 else:
